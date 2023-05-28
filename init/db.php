252117b6f989eeae9d43b476d123e5b42d273d1e
yessentay01 <ye.kurmanbay@iitu.edu.kz>
2023-05-28 21:00:24 +0600

    config fix

--- init/db.php
+++ init/db.php
@@ -5,9 +5,10 @@
 		//$connection = new PDO("mysql:host=localhost;dbname=intranet;charset=utf8", "root", "root");
         //$connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "p-321368_intranet", "p-321368_intranet");
         //$connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "root", "");
+        $connection = new PDO("mysql:host=srv-pleskdb18.ps.kz:3306;dbname=qcodeaca_database;charset=utf8", "qcodeaca_admin", "vOj9En@99");
 
 
-        $connection = new PDO("mysql:host=localhost;dbname=laravel;charset=utf8", "laraveluser", "password");
+//        $connection = new PDO("mysql:host=localhost;dbname=laravel;charset=utf8", "laraveluser", "password");
         //$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
         $connected = true;
--- models/functions.php
+++ models/functions.php
@@ -5,7 +5,9 @@
         //$base_url = "http://intranet.bitlab.kz/";
         //$base_url = "https://intranet.bitlab.kz/";
         //$base_url = "https://intranet.webzone.kz/";
-        $base_url = "http://195.49.210.140/";
+        //$base_url = "http://195.49.210.140/";
+        //$base_url = "http://localhost/rebuild/";
+        $base_url = "https://qcodeacademy.kz/";
 
         if(isset($uri)){
             
