f8c28dab26e0dc4e6e62e816c35a3195b6eded28
yessentay01 <ye.kurmanbay@iitu.edu.kz>
2023-05-28 18:04:29 +0600

    config fix

--- init/db.php
+++ init/db.php
@@ -1,5 +1,5 @@
 <?php
-
+    require_once('db_credentials.php');
 	try {
 	
 		//$connection = new PDO("mysql:host=localhost;dbname=intranet;charset=utf8", "root", "root");
@@ -7,7 +7,8 @@
         //$connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "root", "");
 
 
-        $connection = new PDO("mysql:host=localhost;dbname=laravel;charset=utf8", "laraveluser", "password");
+      //  $connection = new PDO("mysql:host=localhost;dbname=laravel;charset=utf8", "laraveluser", "password");
+        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
         $connected = true;
 	    
---
+++ init/db_credentials.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "laraveluser");
+define("DB_PASS", "password");
+define("DB_NAME", "laravel");
+?>
