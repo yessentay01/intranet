736934b7b713b27fc5101b7619cef9fefdc17a3e
yessentay01 <ye.kurmanbay@iitu.edu.kz>
2023-05-28 17:59:26 +0600

    config fix

--- init/db.php
+++ init/db.php
@@ -5,8 +5,14 @@
 		//$connection = new PDO("mysql:host=localhost;dbname=intranet;charset=utf8", "root", "root");
         //$connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "p-321368_intranet", "p-321368_intranet");
         //$connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "root", "");
-        $connection = new PDO("mysql:host=localhost;dbname=laravel;charset=utf8", "laraveluser", "password");
 
+
+   //     $connection = new PDO("mysql:host=localhost;dbname=laravel;charset=utf8", "laraveluser", "password");
+        $dbhost = "localhost";
+        $dbuser = "laraveluser";
+        $dbpass = "password";
+        $db = "laravel";
+        $connection = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $connection -> error);
         $connected = true;
 	    
 	}catch(Exception $e){
