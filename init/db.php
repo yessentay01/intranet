f049f86debf8fb4b85885db21d31c64db704ed81
yessentay01 <ye.kurmanbay@iitu.edu.kz>
2023-05-28 17:40:03 +0600

    config fix

--- init/db.php
+++ init/db.php
@@ -4,7 +4,8 @@
 	
 		//$connection = new PDO("mysql:host=localhost;dbname=intranet;charset=utf8", "root", "root");
         //$connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "p-321368_intranet", "p-321368_intranet");
-        $connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "root", "");
+        //$connection = new PDO("mysql:host=localhost;dbname=p-321368_intranet;charset=utf8", "root", "");
+        $connection = new PDO("mysql:host=localhost;dbname=laravel;charset=utf8", "laraveluser", "password");
 
         $connected = true;
 	    
--- models/functions.php
+++ models/functions.php
@@ -4,8 +4,9 @@
         
         //$base_url = "http://intranet.bitlab.kz/";
         //$base_url = "https://intranet.bitlab.kz/";
-        $base_url = "https://intranet.webzone.kz/";
-        
+        //$base_url = "https://intranet.webzone.kz/";
+        $base_url = "http://195.49.210.140/";
+
         if(isset($uri)){
             
             return $base_url."".$uri;
